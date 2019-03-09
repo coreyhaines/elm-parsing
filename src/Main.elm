27344d67cb9d4bb068873a4f2923b0c2c14421ba
Corey Haines <coreyhaines@gmail.com>
2019-03-09 14:40:43 -0600

    Code for parsing a float

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Parser as P
 
 
 
@@ -14,12 +15,29 @@ import Html.Events exposing (..)
 
 type alias Model =
     { textToParse : String
+    , parsedValue : ParsedValue
     }
+
+
+type alias Point =
+    { x : Float
+    , y : Float
+    }
+
+
+type alias ParseResult a =
+    Result (List P.DeadEnd) a
+
+
+type ParsedValue
+    = None
+    | ParsedFloat (ParseResult Float)
+    | ParsedPoint (ParseResult Point)
 
 
 init : Model
 init =
-    { textToParse = "" }
+    { textToParse = "", parsedValue = None }
 
 
 
@@ -29,12 +47,24 @@ init =
 
 
 type Msg
-    = NoOp
+    = UpdateTextToParse String
+    | ParseFloat
 
 
 update : Msg -> Model -> Model
 update message model =
-    model
+    case message of
+        UpdateTextToParse text ->
+            { model | textToParse = text }
+
+        ParseFloat ->
+            { model | parsedValue = parseFloat model.textToParse }
+
+
+parseFloat : String -> ParsedValue
+parseFloat textToParse =
+    P.run P.float textToParse
+        |> ParsedFloat
 
 
 
@@ -51,7 +81,16 @@ view model =
               span [ class "logo" ] []
             , h1 [] [ text "How does X work" ]
             ]
-        , p [] [ text "Put X Here" ]
+        , div []
+            [ p [] [ text "Parsing Text" ]
+            , textarea [ cols 50, rows 10, onInput UpdateTextToParse ] []
+            , br [] []
+            , button [ onClick ParseFloat ] [ text "Parse Float" ]
+            ]
+        , div []
+            [ p [] [ text "Model" ]
+            , div [] [ text <| Debug.toString model ]
+            ]
         ]
 
 
